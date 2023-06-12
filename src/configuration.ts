d5b73a07ba987101dcd46238d142f1fc02638055
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2023-06-12 12:31:19 +1200

    feat: change to use id instead of name for references

--- src/configuration.ts
+++ src/configuration.ts
@@ -189,7 +189,7 @@ export class StackConfig {
    * @return {String} an export
    */
   getStackExportId = (name: string): string => {
-    return `${this.getSlug()}-${this.getStageName()}-${name}`
+    return `${this.getSlug()}-${this.getStageId()}-${name}`
   }
 
   /**
@@ -198,7 +198,7 @@ export class StackConfig {
    * @eturn {string}
    */
   getBaseResourceId = (): string => {
-    return `${this.getSlug()}-${this.getStageName()}-${this.getStackName()}`
+    return `${this.getSlug()}-${this.getStageId()}-${this.getId()}`
   }
 
   /**
